d7f1f2dffa4081048afa4508ebe2bc8227992fe3
SridharNaveenPA <sridharnaveen1606@gmail.com>
2025-10-14 11:39:31 +0530

    Created a Task GET w home.html

---
+++ demo/src/main/java/com/amigoscode/demo/TaskController.java
@@ -0,0 +1,48 @@
+package com.amigoscode.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/TASK")
+public class TaskController {
+
+    @GetMapping
+    public List<Task> getTasks() {
+        return List.of(
+                new Task(1, "JAVA ASS"),
+                new Task(2, "WT OBS")
+        );
+    }
+}
+
+class Task {
+    private int id;
+    private String name;
+
+    public Task(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
